9c240c4543ada631cc73e8d957414d50a7cc2947
lotusknight <jxzhao36@gmail.com>
2025-06-12 20:19:05 -0400

    New everything is set up for the project.

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+// Basic Express server setup
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
